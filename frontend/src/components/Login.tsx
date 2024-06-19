9872ab3182f5378a890fc85d64109e2ed74db963
Yura.Y <yanchinskiy432@gmail.com>
2024-06-19 11:42:23 +0300

    created login and registration components

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,9 +1,93 @@
-import React from 'react'
+import { LockOutlined } from "@mui/icons-material";
+import {
+  Container,
+  CssBaseline,
+  Box,
+  Avatar,
+  Typography,
+  TextField,
+  Button,
+  Grid,
+} from "@mui/material";
+import { FC, useState } from "react";
+import { Link } from "react-router-dom";
 
-const Login = () => {
+const Login: FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Add login logic here
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <Box
+      sx={{
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundImage: "url(https://asset.gecdesigns.com/img/wallpapers/aesthetic-landscape-reflection-background-hd-wallpaper-sr10012410-1706502139247-cover.webp)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            padding: 3,
+            borderRadius: 2,
+            boxShadow: 1,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoFocus
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleLogin}
+            >
+              Login
+            </Button>
+            
+                <Link to="/register">Register</Link>
+              
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,9 +1,110 @@
-import React from 'react'
-
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
-
-export default Register
+import {
+    Avatar,
+    Box,
+    Button,
+    Container,
+    CssBaseline,
+    Grid,
+    TextField,
+    Typography,
+  } from "@mui/material";
+  import { LockOutlined } from "@mui/icons-material";
+  import { FC, useState } from "react";
+  import { Link } from "react-router-dom";
+  
+  const Register: FC = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const handleRegister = async () => {};
+  
+    return (
+      
+      <Box
+      sx={{
+        height: "100vh",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundImage: "url(https://wallpapershome.com/images/wallpapers/mountains-1920x1080-forest-sunset-25698.jpeg)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+        <Container maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              backgroundColor: "rgba(255, 255, 255, 0.8)",
+              padding: 3,
+              borderRadius: 2,
+              boxShadow: 1,
+            }}
+          >
+            <Typography variant="h5">Register</Typography>
+            <Box sx={{ mt: 3,
+                
+             }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    name="name"
+                    required
+                    fullWidth
+                    id="name"
+                    label="Name"
+                    autoFocus
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                  />
+                </Grid>
+  
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={handleRegister}
+              >
+                Register
+              </Button>
+              
+                  <Link to="/login">Login</Link>
+               
+            </Box>
+          </Box>
+        </Container>
+        </Box>
+    );
+  };
+  
+  export default Register;
