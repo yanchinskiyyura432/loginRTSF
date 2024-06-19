bc251f7983ecc17a715871f9e2da79e8cd7546d9
Yura.Y <yanchinskiy432@gmail.com>
2024-06-19 11:13:52 +0300

    setup react, ts, router, mui

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/register" element={<Register/>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
